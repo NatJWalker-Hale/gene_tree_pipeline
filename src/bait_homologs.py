148b62d09864f979ea2248b6ee2403340eaaeaa4
NatJWalker-Hale <nathanael.walker.hale@gmail.com>
2020-08-13 13:59:39 +0100

    various updates including changing the shlex tokenizer to accept '?' in tip labels (very rare bug case

--- src/bait_homologs.py
+++ src/bait_homologs.py
@@ -10,13 +10,14 @@ from utils import parse_fasta
 from search_proteomes import search_proteomes
 from fasta_to_tree import fasta_to_tree
 from trim_tree import trim_tree
-from mask_monophyly import mask_monophyly,get_names_to_exclude
+from mask_monophyly import mask_monophyly, get_names_to_exclude
 from cut_internal_branches import cut_internal_branches
 from write_fasta_from_tree import write_fasta_from_tree
 from tree_utils import get_front_labels
 
-def check_bait_presence(seqlist,tree):
-    with open(tree,"r") as inf:
+
+def check_bait_presence(seqlist, tree):
+    with open(tree, "r") as inf:
         tre = newick3.parse(inf.readline())
     taxa = get_front_labels(tre)
     if set(seqlist).intersection(set(taxa)):
@@ -24,25 +25,30 @@ def check_bait_presence(seqlist,tree):
     else:
         return False
 
-def check_same_tree(tree1,tree2):
-    with open(tree1,"r") as inf1:
+
+def check_same_tree(tree1, tree2):
+    with open(tree1, "r") as inf1:
         tre1 = newick3.parse(inf1.readline())
     taxa1 = get_front_labels(tre1)
-    with open(tree2,"r") as inf2:
+    with open(tree2, "r") as inf2:
         tre2 = newick3.parse(inf2.readline())
     taxa2 = get_front_labels(tre2)
     if taxa1 == taxa2:
         return True
     else:
         return False
-    
-def sub_loop(fa,alner,treeblder,abscut,relcut,nt,ignore=[],mask=True,para=True):
-    fasta_to_tree(fa,nt,alner,treeblder)
-    trim_tree(fa+"."+alner+".aln-cln."+treeblder+".tre",relcut,abscut)
+
+
+def sub_loop(fa, alner, treeblder, abscut, relcut, nt, ignore=[],
+             mask=True, para=True):
+    fasta_to_tree(fa, nt, alner, treeblder)
+    trim_tree(fa+"."+alner+".aln-cln."+treeblder+".tre", relcut, abscut)
     if mask:
         mask_monophyly(fa+"."+alner+".aln-cln."+treeblder+".tre.tt",fa+"."+alner+".aln-cln",para,ignore)
 
-def main_loop(bait,fa,alner,treeblder,abscut,relcut,intcut,mintaxa,nt,ignore=[],mask=True,para=True,iterate=False):
+
+def main_loop(bait, fa, alner, treeblder, abscut, relcut, intcut, mintaxa,
+              nt,ignore=[],mask=True,para=True,iterate=False):
     bait_seqs = [key for key in dict([x for x in parse_fasta(bait)]).keys()]
     fasta_to_tree(fa,nt,alner,treeblder)
     trim_tree(fa+"."+alner+".aln-cln."+treeblder+".tre",relcut,abscut)
--- src/newick3.py
+++ src/newick3.py
@@ -9,7 +9,7 @@ class Tokenizer(shlex):
     def __init__(self, infile):
         shlex.__init__(self, infile)
         self.commenters = ''
-        self.wordchars = self.wordchars+'-.@'
+        self.wordchars = self.wordchars+'-.@?'
         self.quotes = "'"
 
     def parse_comment(self):
@@ -156,13 +156,17 @@ def parse_from_file(filename):
     return tree
 
 if __name__ == "__main__":
-    #import ascii
-    s = "(a:3,(b:1e-05,c:1.3)int:5)root;"
-    s = "(Nematostella_vectensis@146487:0.28570255971561625552,((Bargmannia@124461:0.11671371999896994198,(Nanomia@82407:0.02229037752330314398,(Frillagalma@103872:0.03352004060787890788,Agalma_elegans@38387:0.00723798292127085779):0.01418728724518653582):0.07522769713546764714):0.10356415483616157602,Clytia_hemisphaerica@165314:0.07583460439008869736):0.06930854841339750827,Hydra_magnipapillata@6480:0.08755522366843263016):0.0;"
-    #s = "(a,b,c,d,e,f,g);"
-    n = parse(s)
-    print
-    #print ascii.render(n)
-    print(s)
-    print(to_string(n))
-    #print n.next.back.label
+    # #import ascii
+    # s = "(a:3,(b:1e-05,c:1.3)int:5)root;"
+    # s = "(Nematostella_vectensis@146487:0.28570255971561625552,((Bargmannia@124461:0.11671371999896994198,(Nanomia@82407:0.02229037752330314398,(Frillagalma@103872:0.03352004060787890788,Agalma_elegans@38387:0.00723798292127085779):0.01418728724518653582):0.07522769713546764714):0.10356415483616157602,Clytia_hemisphaerica@165314:0.07583460439008869736):0.06930854841339750827,Hydra_magnipapillata@6480:0.08755522366843263016):0.0;"
+    # #s = "(a,b,c,d,e,f,g);"
+    # n = parse(s)
+    # print
+    # #print ascii.render(n)
+    # print(s)
+    # print(to_string(n))
+    # #print n.next.back.label
+
+    t = parse_from_file(sys.argv[1])
+    print(t)
+    print(to_string(t))
