0777f5a11687b5f6c85418add7e18936397037e6
NatJWalker-Hale <nathanael.walker.hale@gmail.com>
2025-01-31 11:58:30 +0000

    small arg string updates

--- src/bait_homologs.py
+++ src/bait_homologs.py
@@ -43,79 +43,60 @@ if __name__ == "__main__":
         sys.argv.append("-h")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--blast", help="Use blastp for similarity \
-                        search instead of default hmmsearch",
-                        action="store_true")
-    parser.add_argument("--min_bitscore", help="Filter blastp hits with \
-                        bitscore lower than min_bitscore (default 30.0)",
-                        type=float, default=30.0)
-    parser.add_argument("--threshold", help="Filter blastp hits with bitscore \
-                        lower than threshold * max bitscore of query \
-                        (default 0.1)", type=float, default=0.1)
-    parser.add_argument("-a", "--aligner", help="Alignment software to use: \
-                        mafft (auto, default), fsa (defaults to --fast)",
-                        default="mafft")
-    parser.add_argument("-t", "--tree_builder", help="Tree building software \
-                        to use: fasttree (wag, default), raxml-ng (defaults to\
-                        WAG+G)", default="fasttree")
-    parser.add_argument("-tc", "--tip_abs_cutoff", help="Absolute branch \
-                        length cutoff for trimming tips. Tips longer than \
-                        this will be trimmed. Defaults to 1.5", default=1.5)
-    parser.add_argument("-tcf", "--tip_abs_cutoff_final", help="Absolute \
-                        branch length cutoff for trimming tips in the final \
-                        round. Tips longer than this will be trimmed. \
-                        Defaults to 1.0", type=float, default=1.0)
-    parser.add_argument("-rc", "--tip_rel_cutoff", help="Relative branch \
-                        length cutoff for trimming tips. Tips longer than \
-                        this and at least 10x longer than sister will be \
-                        trimmed. Defaults to 1.0", default=1.0)
-    parser.add_argument("-rcf", "--tip_rel_cutoff_final", help="Relative \
-                        branch length cutoff for trimming tips in the final \
-                        round. Tips longer than this and at least 10x longer \
-                        than sister will be trimmed. Defaults to 0.5",
-                        type=float, default=0.5)
-    parser.add_argument("-ic", "--internal_cutoff", help="Branch length \
-                        cutoff for internal branches. Subtrees subtended by \
-                        branches longer than this will be trimmed. Defaults \
-                        to 1.0", default=1.0)
-    parser.add_argument("-icf", "--internal_cutoff_final", help="Branch \
-                        length cutoff for internal branches in the final \
-                        round. Subtrees subtended by branches longer than \
-                        this will be trimmed. Defaults to 0.8", type=float,
-                        default=0.8)
-    parser.add_argument("-mt", "--min_taxa", help="Minimum taxa in a subtree \
-                        to conserve and check for bait presence. Defaults to \
-                        4", default=4)
-    parser.add_argument("-nt", "--threads", help="Number of threads to use. \
-                        Defaults to 2", default=2)
-    parser.add_argument("-m", "--mask", help="If this flag is selected, \
-                        monophyletic masking will be conducted",
-                        action="store_true")
-    parser.add_argument("-mp", "--mask_paraphyly", help="Whether to mask \
-                        paraphyletic sequences while doing monophyletic \
-                        masking", action="store_true")
-    parser.add_argument("-if", "--ignore_file", help="File containing taxon \
-                        names (one per line) to ignore while masking \
-                        monophyletic tips. Defaults masks all taxa",
-                        default=None)
-    parser.add_argument("-po", "--prune_og", help="Whether to extract rooted \
-                        ingroup clades containing baits after first round of \
-                        tree inference (requires OG file, default off) - TBI",
-                        action="store_true")
-    parser.add_argument("-og", "--outgroups", help="File containing outgroup \
-                        taxon labels, one per line - TBI")
-    parser.add_argument("-it", "--iterate", help="how many times to iterate \
-                        tree building and cleaning", type=int, default=3)
-    parser.add_argument("-o", "--output_dir", help="Directory to put output. \
-                        Defaults to current directory", default=os.getcwd())
-    parser.add_argument("-k", "--keep", help="Number of hits to keep (default \
-                        all)", type=int, default=None)
-    parser.add_argument("-dbl", "--dblist", help="Text file with files in \
-                        database_dir to to search, if not all, one per line")
+    parser.add_argument("-b", "--blast", help="Use blastp for similarity search instead of default \
+                        hmmsearch", action="store_true")
+    parser.add_argument("--min_bitscore", help="Filter blastp hits with bitscore lower than \
+                        min_bitscore", type=float, default=30.0, metavar='30.0')
+    parser.add_argument("--threshold", help="Filter blastp hits with bitscore lower than threshold \
+                         * max bitscore of query ", type=float, default=0.1, metavar="0.1")
+    parser.add_argument("-a", "--aligner", help="Alignment software to use: mafft, fsa (defaults \
+                        to --fast)", default="mafft", metavar="mafft")
+    parser.add_argument("-t", "--tree_builder", help="Tree building software to use: fasttree \
+                        (wag), raxml-ng (defaults to WAG+G)", default="fasttree",
+                        metavar="fasttree")
+    parser.add_argument("-tc", "--tip_abs_cutoff", help="Absolute branch length cutoff for \
+                        trimming tips. Tips longer than this will be trimmed", default=1.5,
+                        metavar="1.5")
+    parser.add_argument("-tcf", "--tip_abs_cutoff_final", help="Absolute branch length cutoff for \
+                        trimming tips in the final round. Tips longer than this will be trimmed.",
+                        type=float, default=1.0, metavar="1.0")
+    parser.add_argument("-rc", "--tip_rel_cutoff", help="Relative branch length cutoff for \
+                        trimming tips. Tips longer than this and at least 10x longer than sister \
+                        will be trimmed", default=1.0, metavar="1.0")
+    parser.add_argument("-rcf", "--tip_rel_cutoff_final", help="Relative branch length cutoff for \
+                        trimming tips in the final round. Tips longer than this and at least 10x \
+                        longer than sister will be trimmed", type=float, default=0.5, metavar="0.5")
+    parser.add_argument("-ic", "--internal_cutoff", help="Branch length cutoff for internal \
+                        branches. Subtrees subtended by branches longer than this will be trimmed",
+                        default=1.0, metavar="1.0")
+    parser.add_argument("-icf", "--internal_cutoff_final", help="Branch length cutoff for internal \
+                        branches in the final round. Subtrees subtended by branches longer than \
+                        this will be trimmed", type=float, default=0.8, metavar=0.8)
+    parser.add_argument("-mt", "--min_taxa", help="Minimum taxa in a subtree to conserve and check \
+                        for bait presence", default=4, metavar="4")
+    parser.add_argument("-nt", "--threads", help="Number of threads to use", default=2, metavar="2")
+    parser.add_argument("-m", "--mask", help="If this flag is selected, monophyletic masking will \
+                        be conducted", action="store_true")
+    parser.add_argument("-mp", "--mask_paraphyly", help="Whether to mask paraphyletic sequences \
+                        while doing monophyletic masking", action="store_true")
+    parser.add_argument("-if", "--ignore_file", help="File containing taxon names (one per line) \
+                        to ignore while masking monophyletic tips", default=None)
+    # parser.add_argument("-po", "--prune_og", help="Whether to extract rooted ingroup clades \
+    #                     containing baits after first round of tree inference (requires OG file, \
+    #                     default off) - TBI", action="store_true")
+    # parser.add_argument("-og", "--outgroups", help="File containing outgroup taxon labels, one per \
+    #                     line - TBI")
+    parser.add_argument("-it", "--iterate", help="how many times to iterate tree building and \
+                        cleaning", type=int, default=3, metavar="3")
+    parser.add_argument("-o", "--output_dir", help="Directory to put output", default=os.getcwd(),
+                        metavar="./")
+    parser.add_argument("-k", "--keep", help="Number of hits to keep", type=int, default=None,
+                        metavar="all")
+    parser.add_argument("-dbl", "--dblist", help="Text file with files in database_dir to to \
+                        search, if not all, one per line")
     parser.add_argument("bait", help="FASTA file of baits to search")
-    parser.add_argument("database_dir", help="Path to the database containing \
-                        proteomes to search. Expects file endings of .pep.fa \
-                        or .cdhit")
+    parser.add_argument("database_dir", help="Path to the database containing proteomes to search. \
+                        Expects file endings of .pep.fa or .cdhit")
     args = parser.parse_args()
 
     logging.basicConfig(filename='log.txt', encoding='utf-8',
